99cb7675731805a7ec595ecb14d15a79882b4332
Mikle <beardmikle@gmail.com>
2023-03-15 19:15:00 +0300

    final

--- Schedule Local Notifications/ViewController.swift
+++ Schedule Local Notifications/ViewController.swift
@@ -1,19 +1,92 @@
-//
-//  ViewController.swift
-//  Schedule Local Notifications
-//
-//  Created by beardmikle on 12.03.2023.
-//
-
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        notificationCenter.requestAuthorization(options: [.alert, .sound]) {
+            (permissionGranted, error) in
+            if(!permissionGranted)
+            {
+                print("Permission Denied")
+            }
+        }
     }
+    
+    @IBAction func scheduleAction(_ sender: Any)
+    {
+        notificationCenter.getNotificationSettings { [self] (settings) in
+            
+            DispatchQueue.main.async {
 
-
+                let title = self.titleTextField.text!
+                let message = self.messageTextField.text!
+                let date = self.datePicker.date
+                
+                if(settings.authorizationStatus == .authorized)
+                {
+                    let content = UNMutableNotificationContent()
+                    content.title = title
+                    content.body =  message
+                    
+                    let dateComp = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+                    
+                    let trigger = UNCalendarNotificationTrigger(dateMatching: dateComp, repeats: false)
+                    
+                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                    
+                    self.notificationCenter.add(request) { (error) in
+                        if (error != nil)
+                        {
+                            print("Error" + error.debugDescription)
+                            return
+                        }
+                    }
+                    let ac = UIAlertController(title: "Scheduled Notification", message: "At " + self.formattedDate(date: date), preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OKAY", style: .default, handler: { (_) in}))
+                    self.present(ac, animated: true)
+                }
+                else
+                {
+                    let ac = UIAlertController(title: "Enable Notifications", message: "Check your settings", preferredStyle: .alert)
+                    let goToSettings = UIAlertAction(title: "Settings", style: .default)
+                    { (_) in
+                        guard let settingsURL = URL(string: UIApplication.openSettingsURLString)
+                                else
+                        {
+                            return
+                        }
+                        
+                        if(UIApplication.shared.canOpenURL(settingsURL))
+                        {
+                            UIApplication.shared.open(settingsURL) { (_) in}
+                        }
+                        
+                    }
+                    ac.addAction(goToSettings)
+                    ac.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (_) in}))
+                    self.present(ac, animated: true)
+                }
+            }
+            
+        }
+        
+    }
+    
+    func formattedDate(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "d MMM y HH:mm"
+        return formatter.string(from: date)
+    }
+    
 }
-
